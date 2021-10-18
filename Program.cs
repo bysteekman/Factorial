d77c6da0b7181d29f5fea4b9d5302ef83ff9ea57
Yan <bysteekman@gmail.com>
2021-10-18 22:27:31 +0300

    factorial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int factorial = 25;
+            Console.WriteLine(findFactorial(factorial));
+        }
+        static int findFactorial(int num){
+            if(num == 0){
+                return 1;
+            } else {
+                return findFactorial(num - 1) * num;
+            }
+        }
+    }
+}
